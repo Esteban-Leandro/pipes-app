f4cf6057cc01088d41b6d32000dc8f0d1e7fb82b
Esteban <ext.evilches@servipag.cl>
2023-03-23 18:32:10 -0300

    FEAT(): i18nPlural pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -9,16 +9,28 @@ export class UncommonPageComponent {
 
   public name: string = 'Esteban';
   public gender: 'male'|'female' = 'male';
+
+  public clients: string[] = ['Esteban', 'Tiare', 'Violette', 'Bernanrdo','Yolanda']
+
   public invitationMAp = {
     'male':'invitarlo',
     'female':'invitarla',
   }
 
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos 1 cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.',
+  }
 
   changeClient(): void {
     this.name = 'Tiare';
     this.gender = 'female'; 
   }
   
+  deleteClient(){
+    this.clients.shift();
+  }
 
 }
